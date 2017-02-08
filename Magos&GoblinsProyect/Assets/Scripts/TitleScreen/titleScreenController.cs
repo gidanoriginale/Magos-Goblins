8374ca41716aef218bbd1bf2fa3f396407b791a2
unknown <javier.bufo@AU317.epia.granollers>
2017-02-08 11:49:07 +0100

    Carpeta Editor + Script Title

--- Magos&GoblinsProyect/Assets/Scripts/TitleScreen/titleScreenController.cs
+++ Magos&GoblinsProyect/Assets/Scripts/TitleScreen/titleScreenController.cs
@@ -13,7 +13,7 @@ public class titleScreenController : MonoBehaviour {
 		Vector3 nuevaPosicion = new Vector3 (this.transform.position.x, this.transform.position.y - 30, this.transform.position.z);
 		Debug.Log (nuevaPosicion.y);
 		if (Input.GetKeyDown(KeyCode.DownArrow)) {
-
+			
 			if (nuevaPosicion.y < -135) {
 				this.transform.position = new Vector3 (this.transform.position.x, -75, this.transform.position.z);
 			} else {
