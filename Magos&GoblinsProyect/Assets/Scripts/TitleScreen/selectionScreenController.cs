ff824f5c2839b02e0529f60cac0e4d4361fb67d0
unknown <javier.bufo@AU317.epia.granollers>
2017-02-09 13:25:38 +0100

    - selectionScreenController got cleaner and shorter.

--- Magos&GoblinsProyect/Assets/Scripts/TitleScreen/selectionScreenController.cs
+++ Magos&GoblinsProyect/Assets/Scripts/TitleScreen/selectionScreenController.cs
@@ -1,78 +1,95 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class selectionScreenController : MonoBehaviour {
 
-	new Vector3 posicionInicial;
-	new Vector3 posicionFinal;
-	public GameObject startingMenuObject;
-	public GameObject quitMenuOptions;
+	private GameObject[] menuOptions;
+	public GameObject newGame;
+	public GameObject continueGame;
+	public GameObject settings;
+	public GameObject exitGame;
+	private int selectedIndex = 0;
 
-	void Start () {
-		posicionInicial = new Vector3(this.transform.localPosition.x, startingMenuObject.GetComponent<menuScript> ().startText.transform.localPosition.y, this.transform.localPosition.z);
-		posicionFinal = new Vector3 (this.transform.localPosition.x, startingMenuObject.GetComponent<menuScript> ().exitText.transform.localPosition.y, this.transform.localPosition.z);
+	void Start() {
+		menuOptions = new GameObject[4];
+		menuOptions [0] = newGame;
+		menuOptions[1] = continueGame;
+		menuOptions[2] = settings;
+		menuOptions[3] = exitGame;
 	}
-		
-	void Update () {
 
-		Vector3 posicionActual = new Vector3 (transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
-		Debug.Log (posicionActual.y == quitMenuOptions.transform.localPosition.y);
-		//If the quitMenu from startingMenu is DISABLED
-		if (startingMenuObject.GetComponent<menuScript> ().quitMenu.enabled == false) {
-			if (posicionActual.y == quitMenuOptions.transform.localPosition.y) {
-				this.transform.parent = startingMenuObject.GetComponent<menuScript>().startText.transform.parent;
-				//transform.localPosition = new Vector3 (-150, yesOption.transform.localPosition.y, yesOption.transform.localPosition.z);
-			}
-			//Moves the cursor down. If player tries to go further down when there are no
-			//more options, it'll go back to the starting position (posicionInicial)
-			if (Input.GetKeyDown (KeyCode.DownArrow)) {			
-				Vector3 nuevaPosicion = new Vector3 (this.transform.localPosition.x, this.transform.position.y - 30, this.transform.localPosition.z);
-				if (this.GetComponent<RectTransform> ().localPosition.y > posicionFinal.y) {
-					this.transform.localPosition = new Vector3 (this.transform.localPosition.x, this.transform.localPosition.y - 30, this.transform.localPosition.z);
-				} else {
-					this.transform.localPosition = posicionInicial;
-				}
-			}
-
-			//Moves the cursor up. If player tries to go further up when there are no
-			//more options, it'll go to the last available option (posicionFinal)
-			if (Input.GetKeyDown (KeyCode.UpArrow)) {
-				Vector3 nuevaPosicion = new Vector3 (this.transform.localPosition.x, this.transform.localPosition.y + 30, this.transform.localPosition.z);
-				if (this.GetComponent<RectTransform> ().localPosition.y < posicionInicial.y) {
-					this.transform.localPosition = nuevaPosicion;
-				} else {
-					this.transform.localPosition = posicionFinal;
-				}
-			}
-		//If the quitMenu from startingMenu is ENABLED
-		} else {
-			if (posicionActual.x == -110) {
-				this.transform.parent = quitMenuOptions.transform.parent;
-				transform.localPosition = new Vector3 (quitMenuOptions.transform.localPosition.x -50, quitMenuOptions.transform.localPosition.y, quitMenuOptions.transform.localPosition.z);
-			}
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			selectedIndex = menuSelection(menuOptions, selectedIndex, "down");
 		}
 
-		//Input Intro or Spacebar -> Depending on the position of the cursor, 
-		//it'll call one function or another from the GameObject Starting Menu.
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			selectedIndex = menuSelection(menuOptions, selectedIndex, "up");
+		}
+
 		if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) {
-			//If the quitMenu from startingMenu is disabled
-			if (startingMenuObject.GetComponent<menuScript>().quitMenu.enabled == false) {
-				if (transform.localPosition.y == startingMenuObject.GetComponent<menuScript>().startText.transform.localPosition.y) {
-					startingMenuObject.GetComponent<menuScript> ().StartGame ();
-				}
+			handleSelection();
+		}
+		//Debug.Log (selectedIndex);
+	}
 
-				if (transform.localPosition.y == startingMenuObject.GetComponent<menuScript>().continueText.transform.localPosition.y) {
-					//Function to Load Level
-				}
 
-				if (transform.localPosition.y == startingMenuObject.GetComponent<menuScript>().settingsText.transform.localPosition.y) {
-					//Function to change game settings
-				}
 
-				if (transform.localPosition.y == startingMenuObject.GetComponent<menuScript>().exitText.transform.localPosition.y) {
-					startingMenuObject.GetComponent<menuScript> ().ExitPress ();
-				}
+	private int menuSelection (GameObject[] menuItems, int selectedItem, string direction) {
+
+		if (direction == "up") {
+			if (selectedItem == 0) {
+				selectedItem = menuItems.Length-1;
+			} else {
+				selectedItem -= 1;
 			}
 		}
+
+		if (direction == "down") {
+			if (selectedItem == menuItems.Length-1) {
+				selectedItem = 0;
+			} else {
+				selectedItem += 1;
+			}
+		}
+
+		transform.localPosition = new Vector2 (menuItems[selectedItem].transform.localPosition.x -80, menuItems[selectedItem].transform.localPosition.y);
+
+		return selectedItem;
 	}
+
+	void handleSelection() {
+		//GUI.FocusControl (menuOptions[selectedIndex]);
+		Debug.Log("Selected name: " + menuOptions[selectedIndex].name + " / id: " +selectedIndex);
+	}
+//
+//	function OnGUI ()
+//	{
+//		GUI.SetNextControlName ("Tutorial");
+//		if (GUI.Button(Rect(10,70,170,30), "Button 1 (tutorial,id:0)")) {
+//			selectedIndex = 0;
+//			handleSelection();
+//		}
+//
+//		GUI.SetNextControlName ("Play");
+//		if (GUI.Button(Rect(10,100,170,30), "Button 2 (play, id:1)")) {
+//			selectedIndex = 1;
+//			handleSelection();
+//		}
+//
+//		GUI.SetNextControlName ("High Scores");
+//		if (GUI.Button(Rect(10,130,170,30), "Button 3 (high scores, id:2)")) {
+//			selectedIndex = 2;
+//			handleSelection();
+//		}
+//
+//		GUI.SetNextControlName ("Exit");
+//		if (GUI.Button(Rect(10,160,170,30), "Button 4 (exit, id:3)")) {
+//			selectedIndex = 3;
+//			handleSelection();
+//		}
+//
+//		GUI.FocusControl (menuOptions[selectedIndex]);
+//	}
 }
