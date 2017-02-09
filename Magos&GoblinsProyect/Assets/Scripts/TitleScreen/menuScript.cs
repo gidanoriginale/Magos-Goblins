3e41f3d5f0459bf8c7cb5e522c63f759951ee1d9
JavierBufo-Casa <javitobufo@gmail.com>
2017-02-09 01:06:14 +0100

    More changes to TitleScreen: - Added Exit Button. - Added New Game script. - Few visual improvements.
    TO-DO:
    - Continue Button
    - Settings Button
    - Goblin Cursor (maybe?)

---
+++ Magos&GoblinsProyect/Assets/Scripts/TitleScreen/menuScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+	public Canvas quitMenu;
+	public Button startText;
+	public Button continueText;
+	public Button settingsText;
+	public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		continueText = continueText.GetComponent<Button>();
+		settingsText = settingsText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button>();
+		quitMenu.enabled = false;
+	}
+
+	public void ExitPress() {
+		quitMenu.enabled = true;
+		startText.enabled = false;
+		continueText.enabled = false;
+		settingsText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void NoPress() {
+		quitMenu.enabled = false;
+		startText.enabled = true;
+		continueText.enabled = true;
+		settingsText.enabled = true;
+		exitText.enabled = true;
+	}
+
+	public void StartLevel(){
+		SceneManager.LoadScene ("TestField");
+	}
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+
+}
--- Magos&GoblinsProyect/Assets/Scripts/TitleScreen/selectionScreenController.cs
+++ Magos&GoblinsProyect/Assets/Scripts/TitleScreen/selectionScreenController.cs
@@ -6,37 +6,35 @@ public class selectionScreenController : MonoBehaviour {
 	new Vector3 posicionInicial;
 	new Vector3 posicionFinal;
 
-	// Use this for initialization
 	void Start () {
-		posicionInicial = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
-		posicionFinal = new Vector3 (this.transform.position.x, this.transform.position.y -60, this.transform.position.z);
+		posicionInicial = new Vector3(this.transform.localPosition.x, this.transform.localPosition.y, this.transform.localPosition.z);
+		posicionFinal = new Vector3 (this.transform.localPosition.x, this.transform.localPosition.y -90, this.transform.localPosition.z);
 	}
-
-	// Update is called once per frame
+		
 	void Update () {
 
-		//Debug.Log (transform.localPosition.y);
+//		Debug.Log (posicionInicial.y);
 
 		if (Input.GetKeyDown(KeyCode.DownArrow)) {			
-			Vector3 nuevaPosicion = new Vector3 (this.transform.position.x, this.transform.position.y -30, this.transform.position.z);
-			if (this.GetComponent<RectTransform>().localPosition.y > -135) {
-				this.transform.position = nuevaPosicion;
+			Vector3 nuevaPosicion = new Vector3 (this.transform.localPosition.x, this.transform.position.y -30, this.transform.localPosition.z);
+			if (this.GetComponent<RectTransform>().localPosition.y > -165) {
+				this.transform.localPosition = new Vector3 (this.transform.localPosition.x, this.transform.localPosition.y -30, this.transform.localPosition.z);
 			} else {
-				this.transform.position = posicionInicial;
+				this.transform.localPosition = posicionInicial;
 			}
 		}
 
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			Vector3 nuevaPosicion = new Vector3 (this.transform.position.x, this.transform.position.y +30, this.transform.position.z);
+			Vector3 nuevaPosicion = new Vector3 (this.transform.localPosition.x, this.transform.localPosition.y +30, this.transform.localPosition.z);
 			if (this.GetComponent<RectTransform>().localPosition.y < -75) {
-				this.transform.position = nuevaPosicion;
+				this.transform.localPosition = nuevaPosicion;
 			} else {
-				this.transform.position = posicionFinal;
+				this.transform.localPosition = posicionFinal;
 			}
 		}
 
 		if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) {
-			if (transform.localPosition.y == -35) {
+			if (transform.localPosition.y == -75) {
 				Debug.Log ("aaa");
 			}
 		}
