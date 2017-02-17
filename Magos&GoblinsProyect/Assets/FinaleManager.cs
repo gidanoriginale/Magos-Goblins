2cd14097baa1446c48e371e8d67cc2dd31eb7a8f
unknown <alexandre.valle@AU316.epia.granollers>
2017-02-17 11:57:20 +0100

    Final + battles

---
+++ Magos&GoblinsProyect/Assets/FinaleManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FinaleManager : MonoBehaviour {
+
+	public Text introText;
+	private bool changeText=false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown(0) || Input.GetKeyDown (KeyCode.Return)) {
+
+			introText.text = "Unfortunately, this trial version ends here. Sorry but you won't receive the reward... At least for now.";
+			Invoke ("nextPartOk", 0.5f);
+
+			if (changeText) {
+
+				GameObject partyManager = GameObject.Find ("PartyManager");
+				if (partyManager != null) {
+					DontDestroyOnLoad (partyManager);
+				}
+				SceneManager.LoadScene ("ThanksForPlayingIt");
+			}
+		}
+
+	}
+
+	void nextPartOk(){
+
+		changeText = true;
+	}
+
+}
---
+++ Magos&GoblinsProyect/Assets/Scripts/PartyGenerator1.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class PartyGenerator1 : MonoBehaviour {
+
+	public GameObject warriorPlayer;
+	public GameObject roguePlayer;
+	public GameObject magePlayer;
+	public GameObject archerPlayer;
+	public GameObject clericPlayer;
+
+	public GameObject posicion1;
+	public GameObject posicion2;
+	public GameObject posicion3;
+	public GameObject posicion4;
+
+	public ArrayList partyGen;
+	public GameObject unitParent;
+	private GameObject[] enemies;
+	public GameObject Canvas;
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject partyManager = GameObject.Find ("PartyManager");
+		if (partyManager != null) {
+
+			partyGen = partyManager.GetComponent<PartyManager> ().party;
+
+			GameObject firstMember = Instantiate ((GameObject)partyGen [0], new Vector2 (posicion1.transform.position.x,posicion1.transform.position.y),transform.rotation) as GameObject;
+			firstMember.transform.parent = unitParent.transform;
+
+			GameObject secondMember = Instantiate ((GameObject)partyGen [1], new Vector2 (posicion2.transform.position.x,posicion2.transform.position.y),transform.rotation) as GameObject;
+			secondMember.transform.parent = unitParent.transform;
+
+			GameObject thirdMember = Instantiate ((GameObject)partyGen [2], new Vector2 (posicion3.transform.position.x,posicion3.transform.position.y),transform.rotation) as GameObject;
+			thirdMember.transform.parent = unitParent.transform;
+
+			GameObject forthMember = Instantiate ((GameObject)partyGen [3], new Vector2 (posicion4.transform.position.x,posicion4.transform.position.y),transform.rotation) as GameObject;
+			forthMember.transform.parent = unitParent.transform;
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+
+		if (enemies.Length < 1) {
+
+			Invoke ("NextLvl", 1.5f);
+			Canvas.GetComponent<Canvas> ().enabled = true;
+		}
+	}
+
+	void NextLvl() {
+
+		GameObject partyManager = GameObject.Find ("PartyManager");
+		if (partyManager != null) {	
+
+			DontDestroyOnLoad (partyManager);
+		}
+		SceneManager.LoadScene ("Finale");
+	}
+}
